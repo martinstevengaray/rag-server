c20ee3d2e97f2a799def6105ccfdd1fbb16a88ee
mgaray <martinstevengaray@gmail.com>
2026-07-23 07:28:30 -0700

    outline for S3VectorStore

---
+++ src/main/java/com/mgaray/ragserver/bootstrap/S3VectorStore.java
@@ -0,0 +1,16 @@
+package com.mgaray.ragserver.bootstrap;
+
+import java.util.List;
+
+public class S3VectorStore {
+
+    public void add(float[] vector, Object object) {
+        //todo
+    }
+
+
+    public List<Object> get(float[] searchVector, int count) {
+        //todo
+        return null;
+    }
+}
