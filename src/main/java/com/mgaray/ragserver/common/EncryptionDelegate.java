7be2e025c78e7c1d0ed2ba64de852e8687def892
mgaray <martinstevengaray@gmail.com>
2026-07-25 19:43:03 -0700

    deleting dead code, EncryptionUtilsOriginal

--- src/main/java/com/mgaray/ragserver/common/EncryptionDelegate.java
+++ src/main/java/com/mgaray/ragserver/common/EncryptionDelegate.java
@@ -1,11 +1,13 @@
 package com.mgaray.ragserver.common;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
@@ -22,6 +24,12 @@ public class EncryptionDelegate {
     public EncryptionDelegate(String symmetricSigningKey) {
          this.secretKey = new SecretKeySpec(Base64.getDecoder().decode(symmetricSigningKey), "AES");
     }
+
+//    public static SecretKey generateKey() throws GeneralSecurityException {
+//        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+//        keyGenerator.init(KEY_SIZE_BITS);
+//        return keyGenerator.generateKey();
+//    }
 
     public String encrypt(String plaintext) {
         try {
